def624673b9481ed3cd126089fe0e1ba88079a00
zzq <1101921989@qq.com>
2025-12-12 15:06:22 +0800

    初始化提交

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,16 @@
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  );
+import '@/app/ui/global.css'
+import { inter } from "@/app/ui/fonts"
+
+/**
+ * 根框架组件
+ * @param children
+ * @constructor
+ */
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+	console.log('刷新了嘛')
+	return (
+		<html lang="en">
+			<body className={ `${ inter.className } antialiased` }>{ children }</body>
+		</html>
+	);
 }
--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -6,6 +6,7 @@ import { PowerIcon } from '@heroicons/react/24/outline';
 export default function SideNav() {
   return (
     <div className="flex h-full flex-col px-3 py-4 md:px-2">
+      {/* 按钮链接 */}
       <Link
         className="mb-2 flex h-20 items-end justify-start rounded-md bg-blue-600 p-4 md:h-40"
         href="/"
@@ -15,6 +16,7 @@ export default function SideNav() {
         </div>
       </Link>
       <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
+        {/* 写死配置的按钮跳转组件 */}
         <NavLinks />
         <div className="hidden h-auto w-full grow rounded-md bg-gray-50 md:block"></div>
         <form>
